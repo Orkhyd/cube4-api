6062451fe3c131b19b3f02a878109d00d0e4511b
Marius <marius@debian>
2023-11-27 20:46:11 +0100

    init

---
+++ src/test/java/edu/cesi/cube4/Cube4ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cesi.cube4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cube4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
