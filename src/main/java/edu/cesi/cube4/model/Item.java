5db350af2883baf32e1f5b944095db52af1cff7d
Marius <marius@debian>
2023-12-14 16:07:03 +0100

    fix(ser): hibernate to accept uppercase

--- src/main/java/edu/cesi/cube4/model/Item.java
+++ src/main/java/edu/cesi/cube4/model/Item.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
