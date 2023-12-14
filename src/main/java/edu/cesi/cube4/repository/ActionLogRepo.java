5ed0540960ef6f75a2e4f1349b8dd9f50e997b02
guerrinflorian <guerrinflorian@yahoo.com>
2023-12-14 10:39:19 +0100

    Ajout de mon API

--- src/main/java/edu/cesi/cube4/repository/ActionLogRepo.java
+++
@@ -1,9 +0,0 @@
-package edu.cesi.cube4.repository;
-
-import edu.cesi.cube4.model.ActionLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ActionLogRepo extends JpaRepository<ActionLog,Integer> {
-}
