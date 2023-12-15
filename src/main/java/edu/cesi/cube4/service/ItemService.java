ba7936d7bbe228778142184864e1536ab23a07d1
Marius <marius@debian>
2023-12-15 14:18:49 +0100

    add finditembyid + global error handling for request of page that dont exist

--- src/main/java/edu/cesi/cube4/service/ItemService.java
+++ src/main/java/edu/cesi/cube4/service/ItemService.java
@@ -3,9 +3,12 @@ package edu.cesi.cube4.service;
 import edu.cesi.cube4.model.Item;
 import edu.cesi.cube4.repository.ItemRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ItemService {
@@ -22,5 +25,14 @@ public class ItemService {
 
     public Item saveItem(Item item) {
         return itemRepo.save(item);
+    }
+
+    public ResponseEntity<?> findItemById(Integer id) {
+        Optional<Item> item = itemRepo.findById(id);
+        if (item.isEmpty()) {
+            return new ResponseEntity<>("No item was found for id: "+id, HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>(item, HttpStatus.OK);
+        }
     }
 }
