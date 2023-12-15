5267f2d18a651239daa1b3d3f82925c52ea2ad77
Marius <marius@debian>
2023-12-15 10:35:26 +0100

    feat(auth): Handling unsuccesfull login

--- src/main/java/edu/cesi/cube4/auth/AuthController.java
+++ src/main/java/edu/cesi/cube4/auth/AuthController.java
@@ -4,8 +4,10 @@ import edu.cesi.cube4.repository.AdminRepo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -34,24 +36,32 @@ public class AuthController {
     private AdminRepo adminRepo;
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody AuthDTO.LoginRequest userLogin) throws IllegalAccessException {
-        Authentication authentication =
-                authenticationManager
-                        .authenticate(new UsernamePasswordAuthenticationToken(
-                                userLogin.getUsername().toLowerCase(),
-                                userLogin.getPassword()));
-        SecurityContextHolder.getContext().setAuthentication(authentication);
+    public ResponseEntity<?> login(@RequestBody AuthDTO.LoginRequest userLogin) {
+        try {
+            Authentication authentication = authenticationManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(
+                            userLogin.getUsername().toLowerCase(),
+                            userLogin.getPassword()));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
 
-        AuthUser userDetails = (AuthUser) authentication.getPrincipal();
+            AuthUser userDetails = (AuthUser) authentication.getPrincipal();
+            log.info("Token requested for user :{}", authentication.getAuthorities());
+            String token = authService.generateToken(authentication);
 
+            AuthDTO.Response response = new AuthDTO.Response("User logged in successfully", token);
+            return ResponseEntity.ok(response);
+        } catch (BadCredentialsException e) {
+            log.error("Login failed for user: {}", userLogin.getUsername(), e);
 
-        log.info("Token requested for user :{}", authentication.getAuthorities());
-        String token = authService.generateToken(authentication);
-
-        AuthDTO.Response response = new AuthDTO.Response("User logged in successfully", token);
-
-        return ResponseEntity.ok(response);
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body("Login failed: Incorrect username or password");
+        } catch (Exception e) {
+            log.error("Login error for user: {}", userLogin.getUsername(), e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Login error: Internal server error");
+        }
     }
+
 
     @PostMapping("/signup")
     public ResponseEntity<?> signUp(@RequestBody AuthDTO.SignupRequest signUpRequest) {
